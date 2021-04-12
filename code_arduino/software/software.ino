0db8fa66f5c40c66fe7b32c3d34df2e5d3ccb929
vgrau98 <35843843+vgrau98@users.noreply.github.com>
2021-04-12 13:15:56 +0200

    Add files via upload

--- code_arduino/software/software.ino
+++ code_arduino/software/software.ino
@@ -1,4 +1,6 @@
 
+//###################### Libraries import #############
+
 #include <SoftwareSerial.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,21 +9,41 @@
 #include<Adafruit_GFX.h>
 #include<Adafruit_SSD1306.h>
 
-
+//################## constant BT ##################
 #define rxpin 10
 #define txpin 11
 #define baudrate 9600
+SoftwareSerial BT_serial (rxpin,txpin);
 
+
+//################ constant OLED ############
 #define OLED_RESET -1 
 #define DELAY 200 
+
+//############# constant rotatory encoder  #########
+#define encoder0PinA 3 //clk
+#define encoder0PinB 4 // DT
+#define Switch 2 //switch
 
 Adafruit_SSD1306 display(OLED_RESET); 
 
 
-SoftwareSerial mySerial (rxpin,txpin);
 
 int ADC_pin = 0;
-int data;
+int data = 0;
+
+String R2 = "1000";
+
+const long interval = 1000;
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+
+void write_txt_oled(String s, int x, int y, String color){
+
+
+
+  
+}
 
 
 void setup() {
@@ -29,23 +51,46 @@ pinMode(rxpin, INPUT);
 pinMode(txpin, OUTPUT);
 
 Serial.begin(baudrate);
-mySerial.begin(baudrate);
+BT_serial.begin(baudrate);
 
 display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+
+
+
+//Ecrand de démarrage
 display.display();
 delay(2000);
-
+display.clearDisplay();
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.println("Bonjour !");
+display.display();
+delay(3000);
 
 }
 
 void loop() {
 
-data=analogRead(ADC_pin);
-data = map(data, 0, 1024, 0, 255);
-Serial.println(data);
-mySerial.write(data);
-display.clearDisplay(); 
+currentMillis=millis();
+if((currentMillis-previousMillis) >= interval){
+  data=analogRead(ADC_pin);
+  data = map(data, 0, 1024, 0, 255);
+  BT_serial.write(data);
+}
+if(BT_serial.available()){
+  R2 = (BT_serial.readString());
+  Serial.println(R2);
 
-delay(1000);
+
+}
+
+display.clearDisplay();
+display.setCursor (10, 10);
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.println(R2); 
+display.display();
+
+;
 
 }
